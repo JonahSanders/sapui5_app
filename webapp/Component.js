9bb64c2537398da61d501d6ab2aeeef1c14489d1
Sanders <jonahhsanders@gmail.com>
2024-05-14 10:36:36 -0700

    removed index.js in place of a component configuration

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,12 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent"
-], (UIComponent) => {
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], (UIComponent, JSONModel, ResourceModel) => {
     "use strict";
 
-    return UIComponent.extend("", {
+    return UIComponent.extend("ui5.walkthrough.Component", {
+        metadata : {
+           manifest: "json"
+        },
+
         init(){
             //call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
+            //set data model
+            const oData = {
+                recipient : {
+                    name : "World"
+                }
+            };
+            const oModel = new JSONModel(oData);
+            this.setModel(oModel);
         }
     })
 })
