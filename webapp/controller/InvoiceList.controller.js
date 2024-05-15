21d54c95758732e8f494669bd5812bed442d9291
Sanders <jonahhsanders@gmail.com>
2024-05-15 07:52:30 -0700

    deleted index.js logic and moved into separate views/controllers

---
+++ webapp/controller/InvoiceList.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+    "sap/ui/core/mvc/controller",
+    "sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
+    'use strict';
+
+    return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
+        onInit(){
+            const oViewModel = new JSONModel({
+                currency: "EUR"
+            });
+            this.getView().setModel(oViewModel, "view");
+        }
+
+    });
+    
+});
