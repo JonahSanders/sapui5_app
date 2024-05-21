f2daf7b06d5db7d4b7e6a912d57f674989f3b295
Sanders <jonahhsanders@gmail.com>
2024-05-21 10:16:56 -0700

    added routing configuration and mock server

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,35 +1,39 @@
 sap.ui.define([
-    "sap/ui/core/mvc/controller",
-    "sap/ui/model/json/JSONModel",
-    "../model/formatter",
-    "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
-], function(Controller, JSONModel, formatter, Filter, FilterOperator ) {
-    'use strict';
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"../model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], (Controller, JSONModel, formatter, Filter, FilterOperator) => {
+	"use strict";
 
-    return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
-        formatter: formatter,
-        onInit(){
-            const oViewModel = new JSONModel({
-                currency: "EUR"
-            });
-            this.getView().setModel(oViewModel, "view");
-        },
+	return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
+		formatter: formatter,
 
-        onFilterInvoices(oEvent){
-            //build filter array
-            const aFilter = [];
-            const sQuery = oEvent.getParameter("query");
-            if(sQuery) {
-                aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery))
-            }
+		onInit() {
+			const oViewModel = new JSONModel({
+				currency: "EUR"
+			});
+			this.getView().setModel(oViewModel, "view");
+		},
 
-            //filter binding
-            const oList = this.byId("invoiceList");
-            const oBinding = oList.getBinding("items");
-            oBinding.filter(aFilter);
-        }
+		onFilterInvoices(oEvent) {
+			// build filter array
+			const aFilter = [];
+			const sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
 
-    });
-    
+			// filter binding
+			const oList = this.byId("invoiceList");
+			const oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
+		},
+
+		onPress() {
+			const oRouter = this.getOwnerComponent().getRouter();
+			oRouter.navTo("detail");
+		}
+	});
 });
